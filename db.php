dddf338699cbb2477b97cb9012d55a2ad50fff4c
Ian Dunn <padawian@gmail.com>
2025-12-29 12:12:38 -0600

    Refactor database connection settings
    Updated database connection parameters to use placeholders for user input.

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
   
-$host = 'localhost:8889';
-$db_name = 'intentionality';
-$db_user = 'root';
-$db_pass = 'root';
+$host = 'localhost';
+$db_name = '[Your DB Name]';
+$db_user = '[Your DB Username]';
+$db_pass = '[Your DB Password]';
 $charset = 'utf8mb4';
   
 $dsn = "mysql:host=$host;dbname=$db_name;charset=$charset";
@@ -12,5 +12,5 @@ $options = [
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
 ];
+
 $pdo = new PDO($dsn, $db_user, $db_pass, $options);
-?>
