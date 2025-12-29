cf5d29292f52f5949f7b95af1eacab9d739fe872
Ian Dunn <padawian@gmail.com>
2025-12-29 12:11:11 -0600

    Refactor allocation logic in allocation_backend.php

--- allocation_backend.php
+++ allocation_backend.php
@@ -2,15 +2,13 @@
 session_start();
 include('db.php');
 
-if (isset($_POST['allocationSubmit'])) {
-    //Variable initialization
+if(isset($_POST['allocationSubmit'])){
     $needsPrcnt = $_POST["needsPrcnt"];
     $savingsPrcnt = $_POST["savingsPrcnt"];
     $investPrcnt = $_POST["investPrcnt"];
     $wantsPrcnt = $_POST["wantsPrcnt"];
     $username = $_SESSION['user'];
 
-    //Checks if the percentages' sum is not 100 and if any percentages are negative
     if($needsPrcnt + $savingsPrcnt + $investPrcnt + $wantsPrcnt != 100){
         header("Location: allocation.php?error=Percentages must add up to 100");
         exit();
@@ -20,7 +18,6 @@ if (isset($_POST['allocationSubmit'])) {
         exit();
     }
     else{
-        //Adds the new budget percentages to the database and updates session variables
         $stmt = $pdo->prepare("UPDATE users SET `needs%` = '$needsPrcnt', `savings%` = '$savingsPrcnt', 
         `invest%` = '$investPrcnt', `wants%` = '$wantsPrcnt' WHERE user = '$username'");
 	    $stmt->execute();
