fe9ed688c000f04ac6dcdd4e4e793313c8831456
Ian Dunn <padawian@gmail.com>
2025-12-29 12:14:44 -0600

    Update signup page layout and branding

--- signup.php
+++ signup.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Signup</title>
         <link rel="stylesheet" type="text/css" href="style.css">
+        <nav class="navbar">
+            <div class="navdiv">
+                <div class="logo"><a href="#">dfy</a></div>
+                <ul>
+                    <li><a href="index.php">Log In</a></li>
+                </ul>
+            </div>
+        </nav>
     </head>
     <body>
         <form action="signup_backend.php" method="post">
-            <h1>Welcome to Intentionality! Please sign up.</h1>
+            <h1>Welcome to dontforgetwhy! Please sign up.</h1>
             <?php if(isset($_GET['error'])){ ?>
                 <p class="error"> <?php echo $_GET['error']; ?></p>
             <?php } ?>
@@ -17,8 +26,7 @@
             <input type="text" name="username" placeholder="Username"><br>
             <label>Password</label>
             <input type="password" name="password" placeholder="Password"><br>
-            <button type="submit" name="signup">Sign up</button>
-            <a href="index.php">Login</a>
+            <button class="button-style" type="submit" name="signup">Sign up</button>
         </form>
     </body>
 </html>
